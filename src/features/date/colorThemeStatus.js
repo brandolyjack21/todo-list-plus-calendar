a83a611615837eeb9380a3fbf2bab0be10804fef
brandoly-jack <143424064+brandoly-jack@users.noreply.github.com>
2024-02-09 21:51:22 -0500

    cambie los estilos del boton de temas

--- src/features/date/colorThemeStatus.js
+++ src/features/date/colorThemeStatus.js
@@ -3,7 +3,14 @@ import { createSlice } from "@reduxjs/toolkit";
 const colorThemeStatus = createSlice({
     name: 'colorThemeStatus',
     initialState:{
-        value:{ colorName:'blue', style:'bg-blue-600' }
+        value:{
+            colorName: "blue",
+            style: "bg-blue-600",
+            border: "border-blue-600",
+            text: "text-blue-600",
+            text_100: "bg-blue-100",
+            shadow: "shadow-blue-500/50"
+          }
     },
     reducers:{
         newColor: ( state, action) => {
